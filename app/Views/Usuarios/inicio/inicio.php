4a2e2e7c982464a8ee0f04bb5659e2814a128ac2
Gerson <gerson_silguero@outlook.com>
2022-07-21 19:17:03 -0500

    Se agrego la plantilla

---
+++ app/Views/Usuarios/inicio/inicio.php
@@ -0,0 +1,50 @@
+    <!--  links -->
+        <?php echo view("templates/links");?>
+    <!-- /links -->
+
+<body>
+
+    <!-- Menu desplegable -->
+        <?php echo view("Usuarios/header/header");?>
+    <!-- /Menu desplegable -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="section-admin container-fluid">
+            <div class="row admin text-center">
+                <h1 style="color:#fff">Gerson</h1>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- functions -->
+        <?php echo view("templates/functions");?>
+    <!-- /function -->
+
+
+</body>
+
+</html>
